8d441ac7786463e91f30c69230b8ea481a61c546
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-05-01 15:31:29 +0600

    [fit] icon added

--- lib/src/screens/home/home_page.dart
+++ lib/src/screens/home/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/src/theme/colors/app_colors.dart';
 import 'package:my_portfolio/src/theme/controller/theme_icon_button.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,6 +21,32 @@ class _HomePageState extends State<HomePage> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: themeIconButton(context),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor.withValues(alpha: 0.1),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl:
+                      "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+                  errorWidget:
+                      (context, url, error) =>
+                          Icon(Icons.broken_image_rounded, size: 40),
+                  progressIndicatorBuilder:
+                      (context, url, progress) => CircularProgressIndicator(
+                        value:
+                            (progress.downloaded) / (progress.totalSize ?? 1),
+                      ),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
         ],
