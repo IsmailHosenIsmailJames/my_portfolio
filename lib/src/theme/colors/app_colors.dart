683309dba9e717189648136bbafbbd7f6bb9c563
MD. Ismail Hosen <md.ismailhosenismailjames@gmail.com>
2025-08-07 11:51:06 +0600

    [deleted files] Flutter is not recommended. Remove all files from the project.

--- lib/src/theme/colors/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static Color primary = Colors.green.shade600;
-  static Color secondary = Colors.orange;
-  static Color lightMutedGray = Colors.grey.shade600;
-  static Color darkMutedGray = Colors.grey.shade400;
-}
